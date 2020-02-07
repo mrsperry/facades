1e581e4068ad447748e6765466d08493cf727066
Josh <mrjoshuasperry@gmail.com>
2020-02-06 22:40:20 -0500

    Add hover animation for match buttons

--- projects/lovelotus/js/main.js
+++ projects/lovelotus/js/main.js
@@ -1,10 +1,7 @@
 (() => {
-    for (const conversation of $(".conversation")) {
-        const duration = 150;
-        const hidden = $(conversation).children(".conversation-hidden-background");
-        const background = $(conversation).children(".conversation-background");
-
-        $(conversation).hover(() => {
+    const duration = 200;
+    const applyEvents = (element, hidden, background) => {
+        $(element).hover(() => {
             hidden.show();
             background.stop().fadeOut(duration);
         }, () => {
@@ -12,5 +9,21 @@
                 hidden.hide();
             });
         });
+    };
+
+    for (const button of $(".card button")) {
+        const matchHidden = $(button).children(".card-match-hidden-background");
+        const matchBackground = $(button).children(".card-match-background");
+        applyEvents(button, matchHidden, matchBackground);
+
+        const profileHidden = $(button).children(".card-profile-hidden-background");
+        const profileBackground = $(button).children(".card-profile-background");
+        applyEvents(button, profileHidden, profileBackground);
+    }
+
+    for (const conversation of $(".conversation")) {
+        const hidden = $(conversation).children(".conversation-hidden-background");
+        const background = $(conversation).children(".conversation-background");
+        applyEvents(conversation, hidden, background);
     }
 })();
